491a84f73b8ebe2689551b45415e1ada8973ae93
jacopolegovini <legovini96@gmail.com>
2024-10-20 12:50:00 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import MainCard from './components/MainCard.vue';
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    MainCard,
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <MainCard />
+  </div>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="title">
+      <h1>Guess The Pokemon</h1>
+    </div>
+  </header>
+
+  <main>
+    <div class="pokemon-to-guess">
+      <div class="pokemon-image">
+        <img src="https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/257.png" alt="blaziken">
+      </div>
+      <div class="pokemon-question">
+        <section>
+          Which type is this pokemon?
+        </section>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
