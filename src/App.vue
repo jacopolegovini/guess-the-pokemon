b7f31bfee893e94f8d366ac8ecf1c89b81ae9473
jacopolegovini <legovini96@gmail.com>
2024-11-28 21:47:49 +0100

    wallpaper changed

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,15 @@ export default {
 @use "./style/general.scss" as *;
 
 *:not(.full-page *) {
-  background-image: url('./assets/pxfuel.jpg');
-  background-size: contain;
+  background-image: url('./assets/247919.jpg');
+  background-size: cover;
+  background-position: bottom;
 }
 
 .card-general {
   border-radius: 20px;
   -webkit-box-shadow: 8px 0px 0px 0px #DCD0C0, 0px 8px 0px 0px #B1938B, -8px 0px 0px 0px #4E4E56, 0px 0px 0px 8px #DA635D, 5px 5px 15px 5px rgba(0, 0, 0, 0);
   box-shadow: 8px 0px 0px 0px #375DA9, 0px 8px 0px 0px #C92721, -8px 0px 0px 0px #FFCC01, 0px 0px 0px 8px #555557, 5px 5px 15px 5px rgba(0, 0, 0, 0);
-  background-color: white;
-}
-
-.card-general * {
   background-color: white;
 }
 </style>
